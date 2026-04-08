cd946ab8971e4c5efa2bb0ed445cfab52d27cffc
molcham <130418659+molcham@users.noreply.github.com>
2026-04-08 16:30:34 +0900

    feat:채민 4주차 제출

---
+++ 손채민/4153.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 만약 조건에 따라 입력 그만 받는다 ? while + bool + break
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            }
+
+            int max = Math.max(a, Math.max(b, c));
+            int check;
+
+            if (max == a) {
+                check = b * b + c * c;
+            } else if (max == b) {
+                check = a * a + c * c;
+            } else {
+                check = a * a + b * b;
+            }
+
+            if (check == max * max) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
+    }
+}
---
+++ 손채민/8958.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 테스트 케이스 받기
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<t;i++){
+            String s = br.readLine();
+            int score=0;
+            int sum=0;
+            for (int j=0;j<s.length();j++){
+                
+                // 배열로 굳이 안받고 문자열 메서드 사용 !
+                char ch = s.charAt(j);
+                if(ch=='O'){
+                    score++;
+                    sum=sum+score;
+                }
+                else{
+                    score=0;
+                    sum=sum+score;
+                }
+            }
+        
+        System.out.println(sum);
+            
+        }
+    }
+
+}
